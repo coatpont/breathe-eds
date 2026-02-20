628dc1eb48d22e5322b3b9342b146ba86cf87be8
Markus Haack <mhaack@adobe.com>
2026-02-20 11:59:22 +0100

    fix: init UE files

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -149,4 +149,10 @@ async function loadPage() {
   loadDelayed();
 }
 
+// UE Editor support before page load
+if (/\.(stage-ue|ue)\.da\.live$/.test(window.location.hostname)) {
+  // eslint-disable-next-line import/no-unresolved
+  await import(`${window.hlx.codeBasePath}/ue/scripts/ue.js`).then(({ default: ue }) => ue());
+}
+
 loadPage();
